6f3aa6959cd28c013332dbeddd57b478717ed410
Aaron Harding <aaronh1991@gmail.com>
2024-07-13 16:56:09 +0000

    fix syntax error on 214

--- run.py
+++ run.py
@@ -211,10 +211,8 @@ def start_game():
     Declares the global variables and enters a while
     loop until game over equals true
     """
-    global (
-        correct_guessed_letters, incorrect_guessed_letters,
+    global correct_guessed_letters, incorrect_guessed_letters, \
         randomly_chosen_word, lives_left, game_over
-    )
     correct_guessed_letters = []
     incorrect_guessed_letters = []
     randomly_chosen_word = random_word()
