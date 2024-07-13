ad2b94650fc255696a8b3ff48cb7e648f3ec111d
Aaron Harding <aaronh1991@gmail.com>
2024-07-13 16:53:41 +0000

    fix variables in start_game function

--- run.py
+++ run.py
@@ -211,11 +211,10 @@ def start_game():
     Declares the global variables and enters a while
     loop until game over equals true
     """
-    global correct_guessed_letters
-    global incorrect_guessed_letters
-    global randomly_chosen_word
-    global lives_left
-    global game_over
+    global (
+        correct_guessed_letters, incorrect_guessed_letters,
+        randomly_chosen_word, lives_left, game_over
+    )
     correct_guessed_letters = []
     incorrect_guessed_letters = []
     randomly_chosen_word = random_word()
